2f3ce48167a7fe63eaff69c600ade44bc227be75
Huski-commando <gyan.mu@gmail.com>
2024-02-17 23:08:48 +0530

    Initial Commits and package installation dane

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button.tsx";
+
+const App = () => {
+  return (
+    <div className="uppercase underline mt-20">
+      <Button variant="destructive">App</Button>
+    </div>
+  );
+};
+
+export default App;
